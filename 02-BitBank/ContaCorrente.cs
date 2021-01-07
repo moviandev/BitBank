6bd7eeb6973b9176545e4784577e56a1f83c744c
Matheus Viana <matheus_o_viana@hotmail.com>
2021-01-06 22:41:44 -0300

    added new project to test default values

---
+++ 02-BitBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numeroCC;
+    // ADICIONADO VALOR INICIAL DE 100
+    public double saldo = 100;
+}
+
