82ab404e7d71ad51ce037cb091e5144109d6d829
Matheus Viana <matheus_o_viana@hotmail.com>
2021-01-07 23:21:03 -0300

    added titular

--- 04-BtiBank/Program.cs
+++ 04-BtiBank/Program.cs
@@ -10,6 +10,18 @@ namespace _04_BtiBank
     {
         static void Main(string[] args)
         {
+            Cliente gabriela = new Cliente();
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "dev";
+            gabriela.cpf = "123456789012";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = gabriela;
+
+            Console.WriteLine($"Titular: {conta.titular.nome}");
+
+            Console.ReadLine();
         }
     }
 }
