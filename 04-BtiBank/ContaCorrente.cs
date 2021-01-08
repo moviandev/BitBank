7a60f04b474a652cd28652e8df76ee633fba917a
Matheus Viana <matheus_o_viana@hotmail.com>
2021-01-07 23:17:37 -0300

    added namespaces

---
+++ 04-BtiBank/ContaCorrente.cs
@@ -0,0 +1,44 @@
+﻿namespace _04_BtiBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numeroCC;
+        // ADICIONADO VALOR INICIAL DE 100
+        public double saldo = 100;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
+
