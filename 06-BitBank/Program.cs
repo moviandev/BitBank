11895dbdd5b284b58eb2d2c80a9253f4eed9867e
Matheus Viana <matheus_o_viana@hotmail.com>
2021-01-11 22:47:42 -0300

    added constructors and static members

---
+++ 06-BitBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_BitBank
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1111, 1111);
+            Console.ReadLine();
+        }
+    }
+}
