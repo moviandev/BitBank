7bf87d06a841d8c51b33693919967d75c202e79c
Matheus Viana <matheus_o_viana@hotmail.com>
2021-01-11 22:26:29 -0300

    added getters and setters to all properties

--- 05-BitBank/ContaCorrente.cs
+++ 05-BitBank/ContaCorrente.cs
@@ -1,18 +1,42 @@
-﻿namespace _05_BitBank
+﻿using System;
+
+namespace _05_BitBank
 {
     public class ContaCorrente
     {
-        public Cliente titular;
-        public int agencia;
-        public int numeroCC;
+        private Cliente _titular;
+
+        public Cliente Titular
+        {
+            get
+            {
+                return this._titular;
+            }
+            set
+            {
+                if (value is null)
+                {
+                    Console.WriteLine("You need to set a titular");
+                }
+
+                this._titular = value;
+            }
+        }
+
+        public int agencia { get; set; }
+
+
+
+        public int numeroCC { get; set; }
         // ADICIONADO VALOR INICIAL DE 100
-        private double saldo = 100;
+        // ADICIONAR O _ É UMA FORMA DE SEPARAR O NOME DA PROPRIEDADE É DIFERENTE DO GETTER SETTER
+        private double _saldo = 100;
 
         public double Saldo
         {
             get
             {
-                return saldo;
+                return _saldo;
             }
             set
             {
@@ -21,37 +45,37 @@
                     return;
                 }
 
-                saldo = value;
+                _saldo = value;
             }
         }
 
         public bool Sacar(double valor)
         {
-            if (this.saldo < valor)
+            if (this._saldo < valor)
             {
                 return false;
             }
             else
             {
-                this.saldo -= valor;
+                this._saldo -= valor;
                 return true;
             }
         }
 
         public void Depositar(double valor)
         {
-            this.saldo += valor;
+            this._saldo += valor;
         }
 
         public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (this.saldo < valor)
+            if (this._saldo < valor)
             {
                 return false;
             }
             else
             {
-                this.saldo -= valor;
+                this._saldo -= valor;
                 contaDestino.Depositar(valor);
                 return true;
             }
